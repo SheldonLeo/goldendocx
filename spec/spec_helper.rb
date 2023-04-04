9984dfe5a24bd66ff895d889f9d2614c51f5e8cc
WangYuechen <291943739@qq.com>
2023-04-04 15:47:58 +0800

    [CI] Run both ox and nokogiri xml serializer CIs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,7 +28,7 @@ SimpleCov.start do
 end
 
 require 'goldendocx'
-# Goldendocx.configure { |config| config.xml_serializer = :nokogiri }
+Goldendocx.configure { |config| config.xml_serializer = ENV.fetch('XML_SERIALIZER', :ox).to_sym }
 
 Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
 
